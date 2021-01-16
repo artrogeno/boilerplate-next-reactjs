1dad66cbdd204f50c0947acfbed348b375512235
Arthur Silva <arthur@MacBook-Pro-de-Arthur.local>
2021-01-15 21:16:52 -0800

    feat: add the first component style

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -7,9 +7,14 @@ describe('<Main />', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /component main/i })
+      screen.getByRole('heading', { name: /advanced react/i })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
